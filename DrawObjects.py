42c1f5455718d905e79a88248a98f44496b3a5d2
Berend Zwartsenberg <berend@zwartsenberg.nl>
2018-12-19 17:33:29 -0800

    Add annotate_scatter for scatter plots and update annotate_line

--- DrawObjects.py
+++ DrawObjects.py
@@ -87,9 +87,13 @@ def annotate_line(ax, label, atxval = None, atn = None, text = None, x_off = 1.,
         
     
     #getline
+    found = False
     for line in ax.lines:
         if line.get_label() == label:
+            found = True
             break
+    if not found:
+        raise RuntimeError('Axis label not found')
         
     if atn is None:
         if atxval is None:
@@ -171,6 +175,103 @@ def annotate_line(ax, label, atxval = None, atn = None, text = None, x_off = 1.,
         
     t.set_rotation_mode('anchor')
     t.set_rotation(rotation)
+
+
+def annotate_scatter(ax, label, atxval = None, atn = None, text = None, x_off = 1., y_off = 1., 
+                  rotation = None, textkwargs = {}, unit = 'mm', rotation_avg = 1):
+    """Annotate a label in an axis as text next to the line, on the point specified
+    args:
+        ax: the axes
+        atxval, atn: at least one should be given, atn specifies the bin, atxval specifies the x_val
+        label: the label of the line to look for
+        text: text to annotate (if not given, label is used)
+        x_off,y_off offsets of the text to the point on the curve specified
+        rotation: rotation off the text. If None, use the approximate slope of the curve"""
+        
+    
+    #getline
+    found = False
+    for col in ax.collections:
+        if col.get_label() == label:
+            found = True
+            break
+    if not found:
+        raise RuntimeError('Axis label not found')
+        
+    if atn is None:
+        if atxval is None:
+            raise ValueError('Either atn or atxval must be not None')
+        else:
+            atn = (np.abs(col.get_offsets()[:,0] - atxval)).argmin()
+            
+    #get x,y val:
+    x,y = col.get_offsets()[atn,0], col.get_offsets()[atn,1]
+        
+    if unit == 'mm':
+        x_off *= ax.get_figure().dpi/25.4
+        y_off *= ax.get_figure().dpi/25.4
+    elif unit == 'in':
+        x_off *= ax.get_figure().dpi
+        y_off *= ax.get_figure().dpi
+
+        
+    ## transform offsets to axis values
+    r0,r1 = ax.transData.inverted().transform([(-x_off,-y_off),(0.,0.)])
+    x_off_ax,y_off_ax = r1-r0
+
+    
+    #get text:
+    if text is None:
+        text = label
+        
+    if col.get_facecolors().shape[0] > 1:
+        textcolor = col.get_facecolors()[atn]
+    else:
+        textcolor = col.get_facecolors()[0]
+        
+    
+    ## get color, and add to "std_textkwargs"
+    std_textkwargs = {'fontname' : 'Myriad Pro',
+                      'fontsize' : 10,
+                        'color' : textcolor,
+                        'va' : 'bottom',
+                        'ha' : 'left',
+                        'rotation' : 0.0
+                        }   
+    textkwargs = dict(std_textkwargs, **textkwargs)
+    
+    ##plot text
+    t = ax.text(x + x_off_ax, y + y_off_ax, text, **textkwargs)
+    
+    
+    
+    if rotation is None: #set rotation based on curve
+        bb = t.get_window_extent(renderer=ax.get_figure().canvas.get_renderer())
+        sign = 1. if t.get_ha() == 'left' else -1. #if left aligned, go right, else go left
+        
+#        print('Sign used is ', sign)
+        
+        r0,r1 = ax.transData.inverted().transform([(0.,0.),(sign*bb.width,0)])
+        t_xw_ax = (r1-r0)[0] #get the text offset in coordinate space.
+#        print('width in coordinate space is ', t_xw_ax)
+        
+        t_start_atn = (np.abs(col.get_offsets()[:,0] - (x + x_off_ax))).argmin()
+        t_end_atn = (np.abs(col.get_offsets()[:,0] - (x + x_off_ax + t_xw_ax))).argmin()
+        
+        #could add some averging here?
+        avg = int((rotation_avg - 1)/2)
+        t_yend_ax = np.mean(col.get_offsets()[t_end_atn-avg:t_end_atn+avg+1,1])
+        t_ystart_ax = np.mean(col.get_offsets()[t_start_atn-avg:t_start_atn+avg+1,1])
+        
+        r0,r1 = ax.transData.transform([(x + x_off_ax,t_ystart_ax),(x + x_off_ax + t_xw_ax,t_yend_ax)])
+        
+        rotation = np.arctan2((r1-r0)[1],(r1-r0)[0])*180/np.pi
+
+        
+    t.set_rotation_mode('anchor')
+    t.set_rotation(rotation)
+
+
 
     
 def axis_corner_box(ax, text, bwidth, bheight, loc = 'tl', box_props = {}, text_props = {}, units = 'mm'):
