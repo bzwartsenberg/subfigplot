dcba5ef4e3d96f91f4b656a4f3267c39f9a9ea2f
Berend Zwartsenberg <berendz@cs.ubc.ca>
2021-07-16 16:08:51 -0700

    Update custom maps

--- custom_cmaps.py
+++ custom_cmaps.py
@@ -9,8 +9,14 @@ Created on Wed Jan 31 09:43:42 2018
 from matplotlib.colors import LinearSegmentedColormap
 import matplotlib.pyplot as plt
 import numpy as np
+import palettable
+
+cmap1 = palettable.cartocolors.sequential.Teal_6.mpl_colormap
+scolors1 = palettable.cartocolors.qualitative.Prism_10.mpl_colors
 
 
+
+##light blue green:
 c0 = (255/255.,255/255.,255/255.)
 c1 = (98/255., 138/255., 229/255.)
 c2 = (67/255., 209/255., 85/255.)
@@ -18,6 +24,25 @@ c2 = (67/255., 209/255., 85/255.)
 
 lightbluegreen = LinearSegmentedColormap.from_list('lightbluegreen',[c0,c1,c2])
 
+
+
+teal_steps = 7
+clrs_teal = cmap1(np.linspace(0.,1.,num = teal_steps))
+scolors_sub = [scolors1[i] for i in [3,4,5,6,7,8,9,0]]
+clrs_prism = np.ones((len(scolors_sub), 4))
+clrs_prism[:,0:3] = np.array(scolors_sub)
+TealRainbow = LinearSegmentedColormap.from_list('Tealrainbow1', np.concatenate([clrs_teal, clrs_prism]))
+
+
+
+
+            
+teal_steps = 5
+clrs_teal = cmap1(np.linspace(0.,1.,num = teal_steps))
+scolors_sub = [scolors1[i] for i in [0,9,8,7,6,5]]
+clrs_prism = np.ones((len(scolors_sub), 4))
+clrs_prism[:,0:3] = np.array(scolors_sub)
+TealRainbow2 = LinearSegmentedColormap.from_list('Tealrainbow2', np.concatenate([clrs_teal, clrs_prism]))
 
 
 
@@ -36,3 +61,9 @@ if __name__ == '__main__':
     
     
     ax.axis([0,10,0,10])
+    
+    
+    
+    
+    
+    
